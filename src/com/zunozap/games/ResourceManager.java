cebe5d658c622af43c45dbfb0c9c8ce5b271d493
IsaiahPatton <isaiahpatton1@gmail.com>
2019-10-06 17:33:33 -0400

    Remove broken sound system

--- src/com/zunozap/games/ResourceManager.java
+++ src/com/zunozap/games/ResourceManager.java
@@ -1,19 +1,7 @@
 package com.zunozap.games;
 
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-
 import javax.imageio.ImageIO;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.advanced.AdvancedPlayer;
 
 public class ResourceManager {
 
@@ -27,44 +15,18 @@ public class ResourceManager {
     }
 
     public static void playSound(String path) {
-        InputStream in = ResourceManager.class.getClassLoader().getResourceAsStream("sounds/" + path);
-        System.out.println((in != null) + "=" + in.toString());
-        AudioInputStream audioInputStream;
-        try {
-            //audioInputStream = AudioSystem.getAudioInputStream(in);
-            //Clip clip = AudioSystem.getClip();
-            //clip.open(audioInputStream);
-            //clip.start();
-            
-            URL in2 = ResourceManager.class.getClassLoader().getResource("sounds/" + path);
-            java.applet.AudioClip clip2 = java.applet.Applet.newAudioClip(in2);
-            clip2.play();
-        } catch (Exception e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-
-        try {
-            AdvancedPlayer plr = new AdvancedPlayer(in);
-            plr.play(0, 6);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // TODO: working sound system
     }
 
     public static void playSoundAsync(String path) {
-        new Thread(() -> {
-            playSound(path);
-        }).start();
+        new Thread(() -> playSound(path)).start();
     }
 
 }
 
 class ResourceNotFoundException extends Exception {
 
-    String msg;
     public ResourceNotFoundException(Exception e) {
-        msg = e.getMessage();
         this.setStackTrace(e.getStackTrace());
     }
 
